3421d694d22bd753d32c55c8fa22af09b5a5251a
studentzzang <studentzzang@gmail.com>
2026-06-11 02:10:40 +0900

    d

--- top.v
+++ top.v
@@ -14,29 +14,44 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition"
-// CREATED		"Sun May 31 05:45:14 2026"
+// CREATED		"Thu Jun 11 02:08:32 2026"
 
 module top(
 	clk,
 	reset,
 	data_ready,
+	noise,
+	zero,
 	data_in,
 	tx_serial,
-	data_out
+	baud_tick,
+	data_out,
+	rx_cnt,
+	tx_cnt,
+	tx_sel
 );
 
 
 input wire	clk;
 input wire	reset;
 input wire	data_ready;
-input wire	[7:0] data_in;
+input wire	noise;
+input wire	zero;
+input wire	[3:0] data_in;
 output wire	tx_serial;
-output wire	[7:0] data_out;
+output wire	baud_tick;
+output wire	[3:0] data_out;
+output wire	[2:0] rx_cnt;
+output wire	[2:0] tx_cnt;
+output wire	[1:0] tx_sel;
 
-wire	SYNTHESIZED_WIRE_3;
+wire	[3:0] data_out_ALTERA_SYNTHESIZED;
+wire	SYNTHESIZED_WIRE_0;
 wire	SYNTHESIZED_WIRE_1;
+wire	SYNTHESIZED_WIRE_4;
 
-assign	tx_serial = SYNTHESIZED_WIRE_1;
+assign	tx_serial = SYNTHESIZED_WIRE_0;
+assign	baud_tick = SYNTHESIZED_WIRE_4;
 
 
 
@@ -44,24 +59,34 @@ assign	tx_serial = SYNTHESIZED_WIRE_1;
 baud_gen_	b2v_inst(
 	.clk(clk),
 	.reset(reset),
-	.baud_tick(SYNTHESIZED_WIRE_3));
+	.baud_tick(SYNTHESIZED_WIRE_4));
+
+assign	SYNTHESIZED_WIRE_1 = SYNTHESIZED_WIRE_0 ^ noise;
 
 
-tx	b2v_inst6(
-	.clk(clk),
-	.reset(reset),
-	.baud_tick(SYNTHESIZED_WIRE_3),
-	.data_ready(data_ready),
-	.data_in(data_in),
-	.tx_serial(SYNTHESIZED_WIRE_1));
-
-
-rx	b2v_inst7(
+rx	b2v_inst3(
 	.clk(clk),
 	.rx_serial(SYNTHESIZED_WIRE_1),
 	.reset(reset),
-	.baud_tick(SYNTHESIZED_WIRE_3),
-	.data_out(data_out));
+	.baud_tick(SYNTHESIZED_WIRE_4),
+	.zero(zero),
+	.data1(data_out_ALTERA_SYNTHESIZED[0]),
+	.data2(data_out_ALTERA_SYNTHESIZED[1]),
+	.data3(data_out_ALTERA_SYNTHESIZED[2]),
+	.data4(data_out_ALTERA_SYNTHESIZED[3]),
+	.rx_cnt(rx_cnt));
 
+
+tx	b2v_inst5(
+	.clk(clk),
+	.reset(reset),
+	.baud_tick(SYNTHESIZED_WIRE_4),
+	.data_ready(data_ready),
+	.data_in(data_in),
+	.tx_serial(SYNTHESIZED_WIRE_0),
+	.tx_cnt(tx_cnt),
+	.tx_sel(tx_sel));
+
+assign	data_out = data_out_ALTERA_SYNTHESIZED;
 
 endmodule
